bd920cc18802ad6df767a97d2ed7f202a949195e
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2025-01-15 16:59:55 +0000

    added menu items to menu page

--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ export const  homePage = () => {
     const h1 = document.createElement("h1");
     h1.id  = "restaurant-title";
     h1.textContent = "Falcon's Fabolous Taverna"
-
+    
     const p  = document.createElement("p");
     p.id= "restaurant-description";
     p.textContent = "Welcome to Falcon's Fabolous Taverna, opened in the 80s in Holborn, a staple of Greek quisine in London";
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,32 @@
 export const menu = () => {
-  
-    const content = document.querySelector(".content")
+    
+    const content = document.querySelector(".content");
+    const menuPage= document.createElement("div");
+    menuPage.classList = "menu-page";
+
+    const foods = document.createElement("div");
+    foods.className = "foods";
+
+
+    const createItem = (name, desc, price)=> {
+        const foodItem = document.createElement("div");
+        foodItem.className = ".food-card";
+        const foodName = document.createElement("h2");
+        foodName.textContent = name;
+        const foodDesc = document.createElement("p");
+        foodDesc.textContent = desc;
+        const foodPrice = document.createElement("p");
+        foodPrice.textContent = price;
+        foodItem.append(foodName, foodDesc,foodPrice);
+
+        return foodItem;
+    }
+
+    foods.append(createItem("Mousaka", "Tasty greek Mousaka",  "£12.00"));
+    foods.append(createItem("Pita Gyros",  "Greek pita wrap made with pork gyros, tzatziki, tomatoes, and fries", "£8.00"));
+    foods.append(createItem("Pastitsio" , "Greek style lasagna", "£12.00"));
+    foods.append(createItem("Ceasar Salad", "Authentic Greek chicken Ceasar salad ", "£10.00"));
+
     content.textContent = "";
     const h1 = document.createElement("h1");
     h1.id  = "menu-title";
@@ -9,6 +35,7 @@ export const menu = () => {
     const p  = document.createElement("p");
     p.id= "menu-description";
     p.textContent = "We have our menu";
-    content.append(h1, p);
+    menuPage.append(h1,p, foods);
+    content.append(menuPage );
   
 }
